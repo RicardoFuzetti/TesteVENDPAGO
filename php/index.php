bef935b0ec062fb65b9a5eb3e9396bcb74e5c5db
Ricardo Fuzetti <rickfuzetti12@gmail.com>
2022-05-12 13:45:01 -0300

    ajustes

--- php/index.php
+++ php/index.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/main.css">
-    <link rel="stylesheet" href="../style/global.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -142,7 +141,8 @@
                     }
 
                     $hoje = date('Y/m/d');
-                    // $entrega = date('Y/m/d', strtotime("+7 days", strtotime($hoje)));
+                    $entrega = date('Y/m/d', strtotime("+7 days", strtotime($hoje)));
+                    $entregaUsuario = date('d/m/Y', strtotime("+7 days", strtotime($hoje)));
                     $hojeUsuario = date('d/m/Y');
                     ?>
 
@@ -153,14 +153,13 @@
                     <label>Data de retirada: <?php echo $hojeUsuario ?></label><br>
                     <?php echo "<input id='inputData' name='dataRetirada' type='hidden' value=' $hoje '>" ?>
 
-
-                    <label>Seleicone a data de entrega:</label>
-                    <input id="inputData" name="dataEntrega" type="date" required>
+                    <label>Data de entrega: <?php echo $entregaUsuario ?></label>
+                    <?php echo "<input id='inputData' name='dataEntrega' type='hidden' value=' $entrega '>" ?>
                 </div>
 
                 <div class="divButtonModal">
-                    <button class="bCancelar" onclick="ModalEmprestimo.close()">Cancelar</button>
-                    <button class="bCadastrar" onclick="ModalEmprestimo.validarCampo()">Emprestar</button>
+                    <button type="button" class="bCancelar" onclick="ModalEmprestimo.close()">Cancelar</button>
+                    <button type="submit" class="bCadastrar" onclick="ModalEmprestimo.validarCampo()">Emprestar</button>
                 </div>
             </form>
         </div>
